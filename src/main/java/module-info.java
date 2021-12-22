6cb27ef4fe34cb0bce4e1e0d4fd2f0eef150c9b3
skywalker <s.vazov@gmail.com>
2021-12-22 22:42:27 +0100

    remove left over dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,6 @@ module com.kagu.edit.jkagu {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.kordamp.bootstrapfx.core;
-
     opens com.kagu.edit.jkagu to javafx.fxml;
     exports com.kagu.edit.jkagu;
 }
